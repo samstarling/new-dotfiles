b222e9f27bb98257579db2fb4dd0680b6b86f2cb
Sam Starling <mail@samstarling.co.uk>
2015-01-03 10:59:08 +0000

    Added vimrc

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# symlinks
 ln -sf $(pwd)/git/ignore ~/.gitignore
 ln -sf $(pwd)/git/config ~/.gitconfig
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
+ln -sf $(pwd)/vim/vimrc ~/.vimrc
+
+# vim directories
+mkdir -f ~/.vim/backups
+mkdir -f ~/.vim/swaps
