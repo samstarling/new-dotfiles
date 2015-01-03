b7c8a4ed931ce546408d30476185f8d1d0326901
Sam Starling <mail@samstarling.co.uk>
2015-01-03 11:44:29 +0000

    Added my custom oh-my-zsh theme

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 # vim directories
 mkdir -p ~/.vim/backups
 mkdir -p ~/.vim/swaps
+rm -rf ~/.vim/colors
+
+# oh-my-zsh directories
+mkdir -p ~/.oh-my-zsh/themes
 
 # symlinks
 ln -sf $(pwd)/git/ignore ~/.gitignore
 ln -sf $(pwd)/git/config ~/.gitconfig
 ln -sf $(pwd)/zsh/zshrc ~/.zshrc
+ln -sf $(pwd)/zsh/theme ~/.oh-my-zsh/themes/samstarling.zsh-theme
 ln -sf $(pwd)/vim/vimrc ~/.vimrc
 ln -sf $(pwd)/vim/colors ~/.vim/colors
