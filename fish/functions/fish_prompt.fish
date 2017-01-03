1821da4fefea8575941aef5021e308cddd50199a
Sam Starling <sam@soundcloud.com>
2017-01-03 13:39:14 +0100

    Nicer prompt colours

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -47,6 +47,7 @@ function fish_prompt
   set -l cyan (set_color -o cyan)
   set -l yellow (set_color -o yellow)
   set -l red (set_color -o red)
+  set -l green (set_color -o green)
   set -l blue (set_color -o blue)
   set -l normal (set_color normal)
 
@@ -55,7 +56,7 @@ function fish_prompt
     set arrow "$red# "
   end
 
-  set -l cwd $cyan(basename (prompt_pwd))
+  set -l cwd $green(basename (prompt_pwd))
 
   set -l repo_type (_repo_type)
   if [ $repo_type ]
