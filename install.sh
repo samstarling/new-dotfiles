ff884a725d202fa3b19c03fe893ba333f9239cc7
Sam Starling <sam@soundcloud.com>
2017-07-17 12:08:50 -0400

    Add fish config

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-# vim directories
+# required directories
 mkdir -p ~/.vim/backups
 mkdir -p ~/.vim/swaps
 rm -rf ~/.vim/colors
-
-# oh-my-zsh directories
 mkdir -p ~/.oh-my-zsh/themes
-
-# fish directories
-mkdir -p ~/.config/fish/functions
 
 # symlinks
 ln -sf $(pwd)/git/ignore ~/.gitignore
@@ -19,8 +14,8 @@ ln -sf $(pwd)/zsh/theme ~/.oh-my-zsh/themes/samstarling.zsh-theme
 ln -sf $(pwd)/vim/vimrc ~/.vimrc
 ln -sf $(pwd)/vim/colors ~/.vim/colors
 ln -sf $(pwd)/tmux/conf ~/.tmux.conf
-ln -sf $(pwd)/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-ln -sf $(pwd)/fish/functions/fish_greeting.fish ~/.config/fish/functions/fish_greeting.fish
+ln -sf $(pwd)/fish/functions ~/.config/fish
+ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
 
 # account for different home dirs
 git config --global core.excludesfile ~/.gitignore
