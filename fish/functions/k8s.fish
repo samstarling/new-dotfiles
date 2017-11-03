c0f5ac333f5758a29aabcf8b1f50168876e05c83
Sam Starling <mail@samstarling.co.uk>
2017-11-03 09:45:20 +0000

    Remove unused files

--- fish/functions/k8s.fish
+++
@@ -1,3 +0,0 @@
-function k8s
-	docker run --rm -ti docker.dev.s-cloud.net/kubectl-k2:latest kubectl --namespace=(basename $PWD) $argv;
-end
--- fish/functions/kubectl-k2.fish
+++
@@ -1,3 +0,0 @@
-function kubectl-k2
-	docker run --rm -ti docker.dev.s-cloud.net/kubectl-k2:latest kubectl $argv;
-end
