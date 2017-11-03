ef9077ad02935b370c6e8f6046ca084ea9f0eea7
Sam Starling <mail@samstarling.co.uk>
2017-11-03 09:42:16 +0000

    Add ssh config

--- install.sh
+++ install.sh
@@ -16,3 +16,4 @@ ln -sf $(pwd)/vim/colors ~/.vim/colors
 ln -sf $(pwd)/tmux/conf ~/.tmux.conf
 ln -sf $(pwd)/fish/functions ~/.config/fish
 ln -sf $(pwd)/fish/config.fish ~/.config/fish/config.fish
+ln -sf $(pwd)/ssh/config ~/.ssh/config
