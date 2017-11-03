3145ed72ec3ddb937f0ecc45c55d6bd281abe571
Sam Starling <mail@samstarling.co.uk>
2017-11-03 09:44:04 +0000

    Tidy up fish config

--- fish/config.fish
+++ fish/config.fish
@@ -8,12 +8,4 @@ set -g -x PATH "$HOME/Development/go/bin" $PATH
 
 # rbenv
 set -g -x PATH "$HOME/.rbenv/shims" $PATH
-
-# heroku
-set -g -x PATH "/usr/local/heroku/bin" $PATH
-
-# soundcloud
-set -g -x USE_CRUN "false"
-
-# rbenv
 rbenv rehash >/dev/null ^&1
