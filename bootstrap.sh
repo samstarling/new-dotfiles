a703582a89606946132a7331882770d5d4b8a47a
Sam Starling <mail@samstarling.co.uk>
2018-07-17 21:16:28 +0200

    Add a bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install Command Line Tools
+xcode-select --install
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install from Brewfile
+brew bundle
