a69cf4625a47e68234b291b8e6f7c1298233f89f
Sam Starling <mail@samstarling.co.uk>
2018-07-24 11:50:46 +0200

    Use dockutil to configure the Dock

--- install.sh
+++ install.sh
@@ -14,3 +14,12 @@ ln -sf $(pwd)/ssh/config ~/.ssh/config
 brew bundle install
 brew bundle cleanup
 brew cleanup
+
+# Sort out the dock
+dockutil --remove all
+defaults write com.apple.dock tilesize -int 45
+dockutil --add /Applications/Things3.app --no-restart
+dockutil --add /Applications/Safari.app --no-restart
+dockutil --add /Applications/iTerm.app --no-restart
+killall Dock
+
